a838b1a2ca05a4605161ff7577570b21819fc1f0
paranoidjk <hust2012jiangkai@gmail.com>
2018-08-07 14:04:49 +0800

    feat: .d.ts 新增继承自 mm 的 api
    手动复制因为目前没有办法合并这两个库的定义

--- index.d.ts
+++ index.d.ts
@@ -120,6 +120,80 @@ declare namespace mm {
    * restore mock
    */
   function restore(): void;
+
+  /************************************************************************/
+  /* below api are both inherited from https://github.com/node-modules/mm/blob/master/index.d.ts */
+  /************************************************************************/
+
+  /**
+   * Mock async function error.
+   */
+  function error(mod: any, method: string, error?: string | Error, props?: object, timeout?: number): typeof mm;
+
+  /**
+   * mock return callback(null, data).
+   */
+  function data(mod: any, method: string, data: any, timeout?: number): typeof mm;
+
+  /**
+   * mock return callback(null, null).
+   */
+  function empty(mod: any, method: string, timeout?: number): typeof mm;
+
+  /**
+   * mock return callback(null, data1, data2).
+   */
+  function datas(mod: any, method: string, datas: any, timeout?: number): typeof mm;
+
+  /**
+   * mock function sync throw error
+   */
+  function syncError(mod: any, method: string, error?: string | Error, props?: object): void;
+  
+  /**
+   * mock function sync return data
+   */
+  function syncData(mod: any, method: string, data?: any): void;
+
+  /**
+   * mock function sync return nothing
+   */
+  function syncEmpty(mod: any, method: string): void;
+
+  /**
+   * remove all mock effects.
+   */
+  function restore(): typeof mm;
+
+  const http: {
+      request: (
+          url: string | RegExp | { url: string, host: string}, 
+          data: any, 
+          headers?: object, 
+          delay?: number,
+      ) => typeof mm,
+      requestError: (
+          url: string | RegExp | { url: string, host: string}, 
+          reqError: string | Error, 
+          resError: string | Error, 
+          delay?: number
+      ) => typeof mm,
+  }
+
+  const https: {
+      request: (
+          url: string | RegExp | { url: string, host: string}, 
+          data: any, 
+          headers?: object, 
+          delay?: number,
+      ) => typeof mm,
+      requestError: (
+          url: string | RegExp | { url: string, host: string}, 
+          reqError: string | Error, 
+          resError: string | Error, 
+          delay?: number
+      ) => typeof mm,
+  }
 }
 
 export type EggMock = typeof mm;
